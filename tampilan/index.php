3837299330441b240cac97d2cbe0d90bab58f819
Adam Inzaghi <muhammad.adaminzaghi@gmail.com>
2024-12-10 14:06:42 +0700

    feat: merging

--- tampilan/index.php
+++ tampilan/index.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['username'])) {
  <meta charset="UTF-8"> 
 
  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
- <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+ <link href="../css/bootstrap.min.css" rel="stylesheet">
 
  <title>Welcome</title> 
 
@@ -32,5 +32,5 @@ if (!isset($_SESSION['username'])) {
  <p><a href="../proses/proses_logout.php">Logout</a></p> 
 
 </body> 
-<script src="../js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="../js/bootstrap.bundle.min.js"></script>
 </html>
